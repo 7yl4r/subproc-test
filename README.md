9926b978980b9c0c71e4b5d27229fe00f43170ba
Tylar Murray <mail@tylar.info>
2021-07-21 16:08:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is a demonstration of how the python subprocess module's run() module behaves under different error conditions from the command being called.
---
+++ subproc-test/subrproc-test_test.py
@@ -0,0 +1,23 @@
+"""
+test class for exploring subprocess behavior
+
+designed to be run with pytest
+"""
+
+# std modules:
+from unittest import TestCase
+from unittest.mock import patch
+from unittest.mock import MagicMock
+import pytest
+
+# tested module(s):
+import subprocess
+
+class Test_Subprocess_Under_Errors(TestCase):
+
+    # tests:
+    #########################
+    def test_error_raises(self):
+        """ subprocess raises error when test fails """
+        with self.assertRaises(TypeError):
+            subprocess.run(["exit 0"])
