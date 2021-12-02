5ef5be79a6f03552e3cf64cfa28b19627ec8b583
Tylar <mail@tylar.info>
2021-12-02 16:17:28 -0500

    try using sys.argv

--- subproc-test/demo_strfy.py
+++ subproc-test/demo_strfy.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
-from argparse import ArgumentParser
-import logging
 import sys
 
 from strfy_subproc_error import subproc_error_wrapper
     
 if __name__ == "__main__":
-  
-    parser = ArgumentParser(description='enter a failing command and see the subprocess output')
-    parser.add_argument('dmp', nargs='+', ...)
-    opts = parser.parse_args()
-    # ' '.join(opts.dmp)
     print(subproc_error_wrapper(
-      opts.dmp
+      sys.argv[1:]
     ))
 
 else:
