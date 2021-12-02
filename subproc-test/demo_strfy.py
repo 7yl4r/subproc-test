97f9348e052a430b29c29c09ceb1b09b1fca6ae9
Tylar <mail@tylar.info>
2021-12-02 16:14:26 -0500

    try generalizing demo

--- subproc-test/demo_strfy.py
+++ subproc-test/demo_strfy.py
@@ -1,4 +1,19 @@
-from strfy_subproc_error import subproc_error_wrapper
+#!/usr/bin/env python
+from argparse import ArgumentParser
+import logging
+import sys
 
-print(subproc_error_wrapper(['ls', '-abeaveaertyuove']))
-# print(strfy_subproc_error(['adfaba']))
+from strfy_subproc_error import subproc_error_wrapper
+    
+if __name__ == "__main__":
+  
+    parser = ArgumentParser(description='enter a failing command and see the subprocess output')
+    parser.add_argument('dmp', nargs='+', ...)
+    opts = parser.parse_args()
+    # ' '.join(opts.dmp)
+    print(subproc_error_wrapper(
+      opts.dmp
+    ))
+
+else:
+    raise AssertionError("CLI must be called as __main__")
